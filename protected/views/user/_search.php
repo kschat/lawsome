abef4cb299493f37264d9ed5714efa093fa99ada
kschat <kyleschattler@gmail.com>
2013-01-14 15:33:46 -0500

    initial commit

---
+++ protected/views/user/_search.php
@@ -0,0 +1,30 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'firstname'); ?>
+		<?php echo $form->textField($model,'firstname',array('size'=>25,'maxlength'=>25)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
